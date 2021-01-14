b20381779bb93939480a6250f4d8704f8686c774
Camilo Vargas <bomilo66@gmail.com>
2021-01-14 13:10:54 -0500

    [readjustment] change from random to actions

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from character import Character
-from functions import attack,random_strength,info
-import random
+from functions import actions
 
 name_player1 = input('Nombre de jugador 1: ')
 name_player2 = input('Nombre de jugador 2: ')
@@ -14,19 +13,12 @@ turn = 0
 while fight:
     if player1.life > 0 and player2.life > 0:
         if turn == 0:
-            attack(player1, player2)
-            player2.life = player2.life - player1.strength
-            random_strength(player1)
-            info(player2)
+            print(f'Es turno de {player1.name}')
+            actions(player1, player2)
             turn = 1
-            # break
-            # fight = False
-            
         else:
-            attack(player2,player1)
-            player1.life = player1.life - player2.strength
-            random_strength(player2)
-            info(player1)
+            print(f'Es turno de {player2.name}')
+            actions(player2, player1)
             turn = 0
     else:
         if player1.life <= 0:
--- functions.py
+++ functions.py
@@ -1,16 +1,43 @@
 import random
-
-def attack(attacker,defender):
-    print(f'Es el Turno de {attacker.name} \n')
-    print(f'{attacker.name} Lanza ataque!')
-    print(f'{defender.name} recive {attacker.strength} de daño')
     
 
-def random_strength(player):
-    player.strength = random.randint(0,50)
+def actions(attacker,defender):
+    print('1) Ataque devil: 1-10')
+    print('2) Ataque medio: 11-20')
+    print('3) Ataque fuerte: 21-50')
+    
+    try:
+        value = int(input('Elige tu ataque: '))
+        if value == 1:
+            print(f'{attacker.name} LANZA ATAQUE DEVIL!')
+            wound = random.randint(1,10)
+            attack(defender, wound)
+            info(defender)
+            
+        elif value == 2:
+            print(f'{attacker.name} LANZA ATAQUE MEDIO!')
+            wound = random.randint(11,20)
+            attack(defender, wound)
+            info(defender)
+            
+        elif value == 3:
+            print(f'{attacker.name} LANZA ATAQUE FUERTE')
+            wound = random.randint(21,50)
+            attack(defender, wound)
+            info(defender)
+            
+        else: 
+            print('Accion invalida')
+        
+    except ValueError:
+        print('ATENCIÓN: Debe ingresar un número entero.')
+            
+def attack(defender, wound):
+    print(f'{defender.name} recive {wound} de daño')
+    defender.life = defender.life - wound
     
 def info(player):
     if player.life < 0:
         print(f'{player.name} ha Perdido!')
     else:
-        print(f'{player.name} tiene {player.life} de vida restante')
+        print(f'{player.name} tiene {player.life} de vida restante \n')
